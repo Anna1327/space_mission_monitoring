0e6075051738289cf3442fc5b5dc62da726446de
Anna1327 <aoi.yatsu@ya.ru>
2026-04-22 02:38:37 +0300

    add route register

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ...core.database import get_db
+from ...schemas.client import ClientResponse, ClientCreate
 from ...services.auth_service import AuthService
 from ...schemas.auth import ClientCredentials, TokenResponse
 
@@ -16,3 +17,13 @@ def login(credentials: ClientCredentials, db: Session = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid client credentials")
 
     return auth_service.create_token(client)
+
+
+@router.post("/register", response_model=ClientResponse)
+def register_client(
+    data: ClientCreate,
+    db: Session = Depends(get_db)
+):
+    from ...services.client_service import ClientService
+    service = ClientService(db)
+    return service.create(data)
