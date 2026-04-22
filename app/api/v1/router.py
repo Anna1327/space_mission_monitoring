6d4b4a56f16d784a913dc36e6fb74e42c57b9a87
Anna1327 <aoi.yatsu@ya.ru>
2026-04-22 05:29:54 +0300

    add route health

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from . import auth, systems, websocket
+from . import health, auth, systems, websocket
 
 router = APIRouter(prefix="/api/v1")
 
+router.include_router(health.router)
 router.include_router(auth.router)
 router.include_router(systems.router)
 router.include_router(websocket.router)
